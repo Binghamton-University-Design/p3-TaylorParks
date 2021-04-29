88061f545b7adce10602f57128e13e00fc87d9c8
TaylorParks <79161026+TaylorParks@users.noreply.github.com>
2021-04-29 15:51:36 -0400

    barn1 and yard-pano edit

--- js/main.js
+++ js/main.js
@@ -4,4 +4,54 @@ $(document).ready(function() {
 
 console.log('hello');
 
+$('#slider-inner').slick({
+    infinite: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
+
+
+
+$( ".pidgeon-close" ).click(function() {
+  $( this ).parent().fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+$( ".pidgeon" ).click(function() {
+  $( ".pidgeon-overlay" ).fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+
+
+$( ".cheese-close" ).click(function() {
+  $( this ).parent().fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+$( ".cheese" ).click(function() {
+  $( ".cheese-overlay" ).fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+
+
+
+$( ".close-icon" ).click(function() {
+  $( this ).parent().fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
+$( ".bird" ).click(function() {
+  $( ".bird-overlay" ).fadeToggle( "slow", "linear" );
+  event.preventDefault();
+  event.stopPropagation();
+});
+
 });
